4efa8bfeed026d92037d93765b4dbe5b323d5e1f
Rushikesh Narawade <narawaderushikesh@gmail.com>
2025-01-25 10:55:53 +0530

    9th day commit

--- Day_09/problem01.java
+++ Day_09/problem01.java
@@ -1,6 +1,9 @@
+import java.util.*;
 class problem01{
     public static void main(String[] args) {
-        String str= "rushikesh";
+
+        //First apporch without using collection
+        String str= "shikesh";
         System.out.print("Unique characters in string:");
         for(int i=0;i<str.length();i++){
             boolean unique=true;
@@ -12,7 +15,29 @@ class problem01{
             }
             if(unique){
                 System.out.print(str.charAt(i));
+                break;
             }
         }
+        System.out.println("\n");
+        //Second approach using set
+        String str1= "shikesh";
+         Map<Character, Integer> map = new LinkedHashMap<>();
+        
+        // Count the frequency of each character
+        for (int i = 0; i < str1.length(); i++) {
+            char ch = str1.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        System.out.println(map);
+
+        // Find the first character with frequency 1
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println("First non-repeated character: " + entry.getKey());
+                break;
+            }
+        }
+
     }
 }
