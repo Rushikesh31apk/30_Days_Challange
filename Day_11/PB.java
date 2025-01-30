b332b51998c41c0909d717781df1bb7210fe56bc
Rushikesh Narawade <narawaderushikesh@gmail.com>
2025-01-30 07:56:12 +0530

    11th day commit

---
+++ Day_11/PB.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class PB{
+    public static String shortenName(String name) {
+        String[] words = name.split("[-\s]");
+        StringBuilder shortName = new StringBuilder();
+        
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                shortName.append(Character.toLowerCase(word.charAt(0)));
+            }
+        }
+        
+        return shortName.toString();
+    }
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine().trim();
+        scanner.close();
+        
+        System.out.println(shortenName(name));
+    }
+
+   
+}
+
