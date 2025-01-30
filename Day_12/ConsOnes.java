f8bdf5d615f0288a74d8a7ca1e84425d6db017aa
Rushikesh Narawade <narawaderushikesh@gmail.com>
2025-01-30 08:34:23 +0530

    12th day commit

---
+++ Day_12/ConsOnes.java
@@ -0,0 +1,22 @@
+class ConsOnes{
+    public static int cnt(int arr[],int n){
+        int count = 0;
+        int temp=0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                temp+=1;
+                if(count<=temp){
+                    count=temp;
+                }
+            }else{
+                temp=0;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1,0,1,1,0,1};
+        int n = arr.length;
+        System.out.println(cnt(arr,n));
+    }
+}
