5b1cb9c6d7675fa91b5f301776c488dee9d73b14
Rushikesh Narawade <narawaderushikesh@gmail.com>
2025-02-07 09:45:16 +0530

    13th day commit

--- Day_13/TwoSum.java
+++ Day_13/TwoSum.java
@@ -1,12 +1,14 @@
+
+import java.util.HashMap;
+
 class TwoSum{
-    //1 Brute force approch 
+    //1 Brute force approch TC= (n^2)
     public static int[] twoSum(int[] nums, int target) {
         int ans[]=new int[2];
         ans[0]=ans[1]=-1;
         int n=nums.length;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if( i ==j) continue;
+            for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){
                     ans[0]=i;
                     ans[1]=j;
@@ -16,6 +18,23 @@ class TwoSum{
         }
         return ans;
     }
+    //2 Optimized approach using HashMap
+    public static int[] twoSum1(int[] nums, int target) {
+        int ans[]=new int[2];
+        ans[0]=ans[1]=-1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                ans[0]=map.get(complement);
+                ans[1]=i;
+                return ans;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
