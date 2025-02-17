8a1c6bdf9c63722481cc2c956ba5fcf8d72740e7
Rushikesh Narawade <narawaderushikesh@gmail.com>
2025-02-17 10:11:47 +0530

    frequency of each number

--- Day_20/freq.java
+++ Day_20/freq.java
@@ -1,15 +1,13 @@
+
+import java.util.HashMap;
+
 class freq{
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 2, 4, 1};
-        int n= arr.length;
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=i;j<n;j++){
-                if(arr[i] == arr[j]){
-                    cnt++;
-                }
-            }
-            System.err.println((i+1)+" found "+cnt+" times");
+        HashMap<Integer,Integer> cnt= new HashMap();
+        for(int i: arr){
+            cnt.put(i, cnt.getOrDefault(i, 0)+1);
         }
+        System.err.println("frequency of each num: "+ cnt);
     }
 }
