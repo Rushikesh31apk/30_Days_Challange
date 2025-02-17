c618c84ac85bf9417cf1671271325fed819cc093
Rushikesh Narawade <narawaderushikesh@gmail.com>
2025-02-17 10:32:52 +0530

    20th day commit

--- Day_20/mergeArr.java
+++ Day_20/mergeArr.java
@@ -2,10 +2,8 @@ class mergeArr{
     public static void main(String[] args) {
         int[] arr1 = {1, 3,4, 5};
         int[] arr2 = {2,2, 4, 6};
-        int n= arr1.length+arr2.length;
-        int merge[]=new int[n];
+        int merge[]=new int[arr1.length+arr2.length];
         int i=0,j=0,k=0;
-
         while(i<arr1.length && j<arr2.length){
             if(arr1[i]<=arr2[j]){merge[k++]=arr1[i++];}
             else{merge[k++]=arr2[j++];}
